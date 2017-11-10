2faf03891391c2a1c54436b2f8a56ea037ddec0c
Josh winterton <32195114+Winterton-J-BPC-Cyan@users.noreply.github.com>
2017-11-10 09:54:55 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+
+<?php
+$number = 1;
+echo "The first number is $number<br>";
+while ($number < 9) {
+    $number = $number + 1;
+}
+if ($number == 9) {
+    echo "Im now a $number<br>";
+    echo "Dammit, I'm like a confused 6";
+}
+?>
+
+</body>
+
+</html>
